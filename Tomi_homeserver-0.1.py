aaaa92fe6cbf63e2a4b028739e23cb6ac5e3366f
tongmi <3343977167@qq.com>
2020-10-25 21:24:06 +0800

    1

--- Tomi_homeserver-0.1.py
+++ Tomi_homeserver-0.1.py
@@ -1,5 +1,5 @@
 #Import some modules.
-import sys,os,socket,time,json
+import sys,os,socket,time,json,_thread
 
 #Initialize logs.
 if os.path.exists("logs")==True:
@@ -13,6 +13,8 @@ logs=open(logs_name,"w+")
 #Initialize the vars.
 Program_name="Tomi_homeserver"
 Program_version=0.1
+Program_ssh=False
+Program_ssh_password="123456"
 information_backup={"hostname":"localhost","port":31317,"version":0}
 information={"hostname":"localhost","port":31317,"version":0}
 keys=list(information.keys())
@@ -30,6 +32,7 @@ Tomi_homeserver 0.1 Help
 -v/-version/-Version        获取版本信息
 -hostname/-h/-ip <hostname>     修改使用的主机名（不修改config.json的配置）
 -port/-Port/-p <port>     修改使用的端口（不修改config.json的配置）
+-ssh/-s/-S      启用远程ssh连接服务器
 
 """
 
@@ -46,7 +49,7 @@ def init():
     #Print some infomation before initializing.
     info("The programs started,initializing...")
     #Import some objects.
-    global information,keys,information_backup,server,server_mode,server_tcp_listen
+    global information,keys,information_backup,server,server_mode,server_tcp_listen,Program_ssh
     #Initialize config.json file.
     if os.path.exists("config.json")==True:
         if os.path.isfile("config.json")==True:
@@ -83,7 +86,8 @@ def init():
             except:
                 err("The port("+sys.argv[i+1]+") was not correct.Please reset.")
                 os._exit(0)
-    #Initialize the socket of server.
+        if sys.argv[i]=="-ssh" or sys.argv[i]=="-s" or sys.argv[i]=="-S":
+            Program_ssh=True
     try:
         server.bind((information["hostname"],information["port"]))
     except socket.gaierror:
@@ -98,6 +102,35 @@ def init():
         pass
     else:
         server.listen(server_tcp_listen)
+    #If ssh service is on,run this codes.
+    if Program_ssh==True:
+        _thread.start_new_thread(ssh,())
+
+#Initialize sh mode to run some commands that you send.
+def ssh():
+    global information
+    server_ssh_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_ssh_socket.bind((information["hostname"],information["port"]+1))
+    server_ssh_socket.listen(server_tcp_listen)
+    info("Ssh service of the server("+information["hostname"]+") was running at the port "+str(information["port"]+1)+".")
+    while True:
+        c,addr=server_ssh_socket.accept()
+        connected_admin_connected(addr)
+        c.send("Please send the admin password to the server！(UTF-8)".encode("utf-8"))
+        recv_password=c.recv(1024).decode("utf-8")
+        if recv_password==Program_ssh_password:
+            connected_admin_logined(addr)
+            c.send("Login successful.You can send some commands to the server and the server will run this codes.Type \'exit\' can cut the connecting.(UTF-8)".encode("utf-8"))
+            while True:
+                recv_code=c.recv(1029).decode("utf-8")
+                if recv_code=="exit":
+                    connected_admin_exit(addr)
+                    break
+                exec(recv_code)
+        else:
+            connected_admin_loginfail(addr,recv_password)
+            c.send("Password error.Cut the connecting.".encode("utf-8"))
+            c.close()
 
 #Define some functions.
 def info(str):
@@ -124,7 +157,30 @@ def connected(addr):
     print(out,end="")
     logs.write(out)
     logs.flush()
-
+def connected_admin_connected(addr):
+    global logs
+    out=time.strftime("%Y-%m-%d %H:%M:%S"+" The admin "+addr[0]+" "+str(addr[1])+" has connected."+"\n",time.localtime())
+    print(out,end="")
+    logs.write(out)
+    logs.flush()
+def connected_admin_logined(addr):
+    global logs
+    out=time.strftime("%Y-%m-%d %H:%M:%S"+" The admin "+addr[0]+" "+str(addr[1])+" has login."+"\n",time.localtime())
+    print(out,end="")
+    logs.write(out)
+    logs.flush()
+def connected_admin_loginfail(addr,recv_password):
+    global logs
+    out=time.strftime("%Y-%m-%d %H:%M:%S"+" The admin "+addr[0]+" "+str(addr[1])+" login failed.He typed password is \'"+recv_password+"\'.\n",time.localtime())
+    print(out,end="")
+    logs.write(out)
+    logs.flush()
+def connected_admin_exit(addr):
+    global logs
+    out=time.strftime("%Y-%m-%d %H:%M:%S"+" The admin "+addr[0]+" "+str(addr[1])+" exited."+"\n",time.localtime())
+    print(out,end="")
+    logs.write(out)
+    logs.flush()
 #Main codes.
 init()
 while True:
