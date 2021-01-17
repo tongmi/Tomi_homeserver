bf46c3bbb6df031d878e687a57aa9aae71230aca
tongmi <3343977167@qq.com>
2021-01-17 13:08:46 +0800

    1

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket as s
+try:
+    c=s.socket()
+except:
+    print("未知错误.\nUnkown error.")
+    exit()
+try:
+    c.connect(("tongmios.iask.in",30577))
+except:
+    print("服务器离线。\nThe server is offline.")
+    c.close()
+    exit()
+str0=c.recv(1024).decode("utf-8")
+print(str0)
+c.close()
+
